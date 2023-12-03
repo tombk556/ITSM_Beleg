da7c348a7ff04dc3a9c53c063e6249decba45549
Tom Bischopink <tom.bischopink@stud.htw-dresden.de>
2023-12-03 10:46:37 +0100

    add filter on get incident request

--- app/routers/incident.py
+++ app/routers/incident.py
@@ -11,9 +11,18 @@ INSTANCE = config.get("INSTANCE_SN")
 USERNAME_SN = config.get("USERNAME_SN")
 PASSWORD_SN = config.get("PASSWORD_SN")
 
-@incident.get("/get_incident")
-def get_incidents():
-    return _incidents(INSTANCE, USERNAME_SN, PASSWORD_SN)
+@incident.get("/get_incident/{type}", status_code=status.HTTP_200_OK)
+def get_incidents(type: str = None):
+    incidents = _incidents(INSTANCE, USERNAME_SN, PASSWORD_SN)
+    
+    if type == "date":
+        return sorted(incidents, key=lambda x: x["date"], reverse=True)
+    elif type == "number":
+        return sorted(incidents, key=lambda x: x["number"], reverse=True) 
+    elif type == "state":
+        return sorted(incidents, key=lambda x: x["state"], reverse=False) 
+    else:
+        return incidents
 
 
 @incident.post("/create_incident", status_code=status.HTTP_201_CREATED, response_model=schemas.Incident)
@@ -36,10 +45,26 @@ def _incidents(instance, user, pwd):
                 "Error Response": response.json()
             }
         else:
-            return response.json()
+            return incident_data(data=response.json())
 
     except Exception as error:
         return {
             "Status": "Intenal Server Error",
             "Error Response": error
         }
+        
+def incident_data(data):
+    incidents = []
+    
+    for result_item in data["result"]:
+        incident = {
+            "number" : result_item.get("number"),
+            "date" : result_item.get("sys_updated_on"),
+            "short_description" : result_item.get("short_description"),
+            "description" : result_item.get("description"),
+            "state" : result_item.get("incident_state")
+        }
+        
+        incidents.append(incident)
+    
+    return incidents

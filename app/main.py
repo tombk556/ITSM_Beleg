75cbd01d03ad308e4255f7d1f966f27e4184e372
Tom Bischopink <tom.bischopink@stud.htw-dresden.de>
2023-12-05 14:52:12 +0100

    added Cors for Frontend

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
 from .routers import incident
+from fastapi.middleware.cors import CORSMiddleware
+ 
 app = FastAPI()
-
-app.include_router(incident.incident)
-
+ 
+# Configure CORS to allow requests from http://localhost:8081
+origins = [
+    "http://localhost:8081",
+    # Add more allowed origins if needed
+]
+ 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+ 
 @app.get("/")
 def root():
     return "Server is running"
+ 
+app.include_router(incident.incident)

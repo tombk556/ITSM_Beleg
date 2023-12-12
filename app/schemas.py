cab2df227d90916866c88f29c465561d7f3d02bc
Tom Bischopink <tom.bischopink@stud.htw-dresden.de>
2023-12-12 13:29:35 +0100

    update

--- app/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Incident(BaseModel):
-    short_description : str
-    description : str
-
-class CreateIncident(Incident):
-    class Config:
-        orm_mode = True

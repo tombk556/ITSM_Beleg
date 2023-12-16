59140a42f282d73d7fc388f2b50a6cf7589c0360
Tom Bischopink <tom.bischopink@stud.htw-dresden.de>
2023-12-16 12:17:02 +0100

    add all domains

--- app/main.py
+++ app/main.py
@@ -3,19 +3,11 @@ from .routers import incident
 from fastapi.middleware.cors import CORSMiddleware
  
 app = FastAPI()
- 
-origins = [
-        "http://localhost",
-        "http://127.0.0.1",
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        "https://beleg.azurewebsites.net"
-        "https://itsmgruppe1.azurewebsites.net"
-    ]
- 
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -26,3 +18,4 @@ def root():
     return "Server is running"
  
 app.include_router(incident.incident)
+

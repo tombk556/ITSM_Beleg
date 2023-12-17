985d5e11e9e8a95ea8b5f497b6c4a87b6700213e
Tom Bischopink <tom.bischopink@stud.htw-dresden.de>
2023-12-17 16:51:52 +0100

    first commit

--- app/routers/incident.py
+++ app/routers/incident.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status, HTTPException
 from ..config import settings
 from .. import schemas
+from datetime import datetime, timedelta
 import requests
 
 incident = APIRouter(
@@ -38,6 +39,10 @@ def get_incident_by_number(number: str):
 def get_incidents_by_state(state: int):
     return _get_incident(INSTANCE, USERNAME_SN, PASSWORD_SN, filter="incident_state", filter_element=state)
 
+@incident.get("/get_incident_by_sys_id/{sys_id}")
+def get_incident_by_number(sys_id: str):
+    return _get_incident(INSTANCE, USERNAME_SN, PASSWORD_SN, filter="sys_id", filter_element=sys_id)
+
 
 def _get_all_incidents(instance, user, pwd):
     headers = {"Content-Type": "application/json",
@@ -61,13 +66,13 @@ def _get_all_incidents(instance, user, pwd):
         }
     return
 
-def _get_incident(instance, user, pwd, filter, filter_element, with_sys_id = False):
+def _get_incident(instance, user, pwd, filter, filter_element):
     headers = {"Content-Type": "application/json",
                "Accept": "application/json"}
-    if filter == "number":
+    if filter == "number" or filter == "sys_id" or filter == "sys_id" or filter == "incident_state":
         url = f"https://{instance}.lab.service-now.com/api/now/table/incident?{filter}={filter_element}"
     else:
-        url = f"https://{instance}.lab.service-now.com/api/now/table/incident?incident_state={filter_element}"
+        url = f"https://{instance}.lab.service-now.com/api/now/table/incident"
         
     try:
         response = requests.get(url, auth=(user, pwd), headers=headers)
@@ -78,7 +83,7 @@ def _get_incident(instance, user, pwd, filter, filter_element, with_sys_id = Fal
                 "Error Response": response.json()
             }
         else:
-            return incident_data(response.json(), with_sys_id)
+            return incident_data(response.json())
 
     except Exception as error:
         return {
@@ -86,29 +91,53 @@ def _get_incident(instance, user, pwd, filter, filter_element, with_sys_id = Fal
             "Error Response": error
         }
 
-def incident_data(data, with_sys_id = False):
+def incident_data(data):
     incidents = []
 
     for result_item in data["result"]:
         incident = {
+            "sys_id": result_item.get("sys_id"),
             "number": result_item.get("number"),
             "date": result_item.get("sys_updated_on"),
             "short_description": result_item.get("short_description"),
             "description": result_item.get("description"),
             "state": result_item.get("incident_state")
         }
-        if with_sys_id:
-            incident.update({"sys_id": result_item.get("sys_id")})
         
         incidents.append(incident)
 
     return incidents
 
 
+@incident.get("/get_incidents_since/{time_data}")
+def get_incident_by_date(time_data: str):
+    data = get_incidents("date")
+
+    if time_data == "yesterday":
+        # Der aktuelle Tag
+        current_day = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+        # Vortag
+        yesterday = current_day - timedelta(days=1)
+
+        filtered_data = [entry for entry in data if datetime.strptime(entry["date"], "%Y-%m-%d %H:%M:%S") >= yesterday]
+        return filtered_data 
+    
+    elif "-" in time_data:
+        try:
+            mydate = datetime.strptime(time_data, "%Y-%m-%d")
+            filtered_data = [entry for entry in data if datetime.strptime(entry["date"], "%Y-%m-%d %H:%M:%S").date() >= mydate.date()]
+            return filtered_data 
+        except ValueError:
+            return {"error": "Ungültiges Datumsformat. Verwenden Sie das Format %Y-%m-%d."}
+    else:
+        return HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                             detail="Ungültiges Datumsformat. Verwenden Sie das Format %Y-%m-%d oder yesterday")
+
 # POST-Methode zur Erstellung neuer Incidents
 
 @incident.post("/create_incident", status_code=status.HTTP_201_CREATED, response_model=schemas.Incident)
-def create_incident(inc: schemas.CreateIncident):
+def create_incident(inc: schemas.CrUpIncident):
     headers = {"Content-Type": "application/json",
                "Accept": "application/json"
             }
@@ -123,29 +152,36 @@ def create_incident(inc: schemas.CreateIncident):
 
     # Überprüfung und Ausgabe
     if response.status_code == 201:
-        return incident_data
+        return _single_incident(response.json())
     else:
         raise HTTPException(status_code=response.status_code, detail=f"Fehler beim Erstellen des Incidents: {response.text}")
 
 
+def _single_incident(data):
+    result_item = data["result"]
+    incident = {
+            "sys_id": result_item.get("sys_id"),
+            "number": result_item.get("number"),
+            "date": result_item.get("sys_updated_on"),
+            "short_description": result_item.get("short_description"),
+            "description": result_item.get("description"),
+            "state": result_item.get("incident_state")
+        }
+    return incident
+
 # PUT-Methode für die Änderung bereits existierender Incidents
-@incident.patch("/update_incident/{number}", status_code=status.HTTP_200_OK, response_model=schemas.Incident)
-def update_incident(inc: schemas.UpdateIncident, number: str):
+@incident.patch("/update_incident/{sys_id}", status_code=status.HTTP_200_OK, response_model=schemas.Incident)
+def update_incident(inc: schemas.CrUpIncident, sys_id: str):
     headers = {"Content-Type": "application/json",
                "Accept": "application/json"
             }
-    url = f"https://{INSTANCE}.lab.service-now.com/api/now/table/incident/{number}"
-
+    
     # Abrufung des Incidents 
-    existing_incident = _get_incident(INSTANCE, USERNAME_SN, PASSWORD_SN, filter="number", filter_element=number, with_sys_id= True)
+    existing_incident = _get_incident(INSTANCE, USERNAME_SN, PASSWORD_SN, filter="sys_id", filter_element=sys_id)
 
     if not existing_incident:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Incident {number} kann nicht gefunden werden!")
-    else:
-        sys_id = existing_incident[0].get("sys_id")
-        print(f"Sys_ID: {sys_id}")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Incident {sys_id} kann nicht gefunden werden!")
 
-     # Nur die zu aktualisierenden Felder im Request-Body senden
     update_data = {
         "description": inc.description,
         "short_description": inc.short_description
@@ -157,9 +193,30 @@ def update_incident(inc: schemas.UpdateIncident, number: str):
 
     # Überprüfen und Ausgabe
     if response.status_code == 200:
-        return update_data
+        return _single_incident(response.json())
     else:
         raise HTTPException(status_code=response.status_code, detail=f"Fehler beim Aktualisieren des Incidents: {response.text}")
     
 
+# DELETE-Methode für das Löschen bereits existierender Incidents
+@incident.delete("/delete_incident/{sys_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_incident(sys_id: str):
+    headers = {"Content-Type": "application/json",
+               "Accept": "application/json"
+            }
 
+    # Abrufung des Incidents 
+    existing_incident = _get_incident(INSTANCE, USERNAME_SN, PASSWORD_SN, filter="sys_id", filter_element=sys_id)
+
+    if not existing_incident:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Incident mit der ID {sys_id} kann nicht gefunden werden!")
+
+    # DELETE-Request an ServiceNow-API
+    url = f"https://{INSTANCE}.lab.service-now.com/api/now/table/incident/{sys_id}"
+    response = requests.delete(url, auth=(USERNAME_SN, PASSWORD_SN), headers=headers)
+
+    # Überprüfen und Ausgabe
+    if response.status_code == 204:
+        return None
+    else:
+        raise HTTPException(status_code=response.status_code, detail=f"Fehler beim Löschen des Incidents: {response.text}")

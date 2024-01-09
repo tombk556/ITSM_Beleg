5094f31c583776979f59e1cd10c8a32188234f1c
Tom Bischopink <tom.bischopink@stud.htw-dresden.de>
2024-01-09 19:47:37 +0100

    frontend: change baseURL to 8000 fastapi

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://itsmgruppe1backend.azurewebsites.net",
+  baseURL: "http://127.0.0.1:8000",
   headers: {
     "Content-type": "application/json"
   }

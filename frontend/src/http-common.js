b813f77d56167713589a805b116013c32d779ac8
Tom Bischopink <tom.bischopink@stud.htw-dresden.de>
2024-01-16 14:14:50 +0100

    commit

--- frontend/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://itsmgruppe1backend.azurewebsites.net",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
--- frontend/src/services/GetIncidentByNumberService.js
+++
@@ -1,9 +0,0 @@
-import http from "@/http-common";
-
-class GetIncidentByNumberService {
-  get(number) {
-    return http.get(`/incident/get_incident_by_number/${number}`);
-  }
-}
-
-export default new GetIncidentByNumberService();

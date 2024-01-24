e40c41afdc4075ac9e99de8fd35cac8014ee7dc6
Tom Bischopink <tom.bischopink@stud.htw-dresden.de>
2024-01-24 14:12:50 +0100

    update

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -6,7 +6,7 @@ defineProps({
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">ITSM Beleg Gruppe 1 - Ayana, Chari, Konrad, Tom</a>
+      <a class="navbar-brand" href="#">ITSM Beleg Wintersemester 23/24 Gruppe 1 - Ayana, Chari Konrad, Tom</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
